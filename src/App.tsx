ad915e82192dcc3b242652cd95f04ea26a507679
khw970421 <khw970421@kakao.com>
2023-05-16 11:25:13 +0900

    Comment  API 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { useEffect, useState } from 'react'
 import { useInView } from 'react-intersection-observer'
-import { useInfiniteQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 
 import HttpInstance from './api/user';
 import { PagesPostType, PostType } from './utils/types';
 import './App.css'
 import { pagePerLimitPosts } from './utils/const';
 import GridPosts from './components/Post/GridPosts';
+import Comments from './components/Comment/Comments';
 
 
 function App() {
   const [ref, inView, entry] = useInView()
   const [posts, setPosts] = useState<PostType[]>([])
+  const [selectedPostId, setSelectedPostId] = useState<string>('')
+
   const { isFetching, fetchNextPage, hasNextPage } = useInfiniteQuery(
     ['infinitePerson'],
     async ({ pageParam = 1 }) => {
@@ -37,15 +40,34 @@ function App() {
     }
   )
 
+  const { data: comments, refetch } = useQuery(['comments'], async () => {
+    const comments = await HttpInstance.getComments(`${selectedPostId}`)
+    return comments.data
+  }, {
+    enabled: !!selectedPostId
+  }
+  )
+
   useEffect(() => {
     if (inView && hasNextPage) {
       fetchNextPage()
     }
   }, [inView, entry?.target])
+
+  const handleSelectedPostId = (postId: string) => {
+    setSelectedPostId(() => postId)
+  }
+
+  useEffect(() => {
+    refetch()
+  }, [selectedPostId])
+
   return (
     <div className="app-wrapper">
       <h3>Posts</h3>
-      <GridPosts posts={posts} bottomRef={ref} isFetching={isFetching} />
+      <Comments comments={comments || []} />
+      <GridPosts posts={posts} bottomRef={ref} isFetching={isFetching} clickSelectedPostId={handleSelectedPostId} />
+
     </div>
   )
 }
