b009023659e0d1fa4f7d93faa2d5f40392d8a4d0
khw970421 <khw970421@kakao.com>
2023-05-17 15:17:23 +0900

    bottom background 기능구현

--- src/components/BottomSheet/ButtonSheet.tsx
+++ src/components/BottomSheet/ButtonSheet.tsx
@@ -10,15 +10,19 @@ const ButtonSheet = (props: ButtonSheetProps) => {
   const { children, className, closeButtonSheet } = props
   const commentLength = children.props?.comments?.length
   return (
-    <div className={`bottom-sheet-wrapper ${className}`}>
-      <header className="bottom-sheet-header">
-        <h3>게시물 댓글 <span>{commentLength}</span></h3>
-        <button className="bottom-sheet-close-btn" onClick={closeButtonSheet}>X</button>
-      </header>
-      <div className="bottom-sheet-content">
-        {children}
-      </div>
-    </div >
+    <div className={`bottom-container ${className}`}>
+      <div className={`bottom-background ${className}`}></div>
+      <div className={`bottom-sheet-wrapper ${className}`}>
+        <header className="bottom-sheet-header">
+          <h3>게시물 댓글 <span>{commentLength}</span></h3>
+          <button className="bottom-sheet-close-btn" onClick={closeButtonSheet}>X</button>
+        </header>
+        <div className="bottom-sheet-content">
+          {children}
+        </div>
+      </div >
+    </div>
+
   );
 };
 
