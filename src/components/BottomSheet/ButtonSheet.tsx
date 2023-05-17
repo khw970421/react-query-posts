3db3ec20d3f6f11571dde4355ee1e2093c0d85da
khw970421 <khw970421@kakao.com>
2023-05-17 15:25:09 +0900

    scss className 수정

--- src/components/BottomSheet/ButtonSheet.tsx
+++ src/components/BottomSheet/ButtonSheet.tsx
@@ -10,9 +10,9 @@ const ButtonSheet = (props: ButtonSheetProps) => {
   const { children, className, closeButtonSheet } = props
   const commentLength = children.props?.comments?.length
   return (
-    <div className={`bottom-container ${className}`}>
-      <div className={`bottom-background ${className}`}></div>
-      <div className={`bottom-sheet-wrapper ${className}`}>
+    <div className={`bottom-sheet-wrapper ${className}`}>
+      <div className={`bottom-sheet-background ${className}`} />
+      <div className={`bottom-sheet-container ${className}`}>
         <header className="bottom-sheet-header">
           <h3>게시물 댓글 <span>{commentLength}</span></h3>
           <button className="bottom-sheet-close-btn" onClick={closeButtonSheet}>X</button>
