04ee575f25a38ee887e8961c255e58af4fbe9cef
khw970421 <khw970421@kakao.com>
2023-05-24 12:02:48 +0900

    불필요 test 코드 제거

--- src/App.test.tsx
+++
@@ -1,7 +0,0 @@
-// import App from "./App.tsx";
-import { render, screen } from '@testing-library/react'
-
-test('Renders main page correctly', () => {
-  render(<div>Hello, Jest!</div>);
-  expect(screen.getByText('Hello, Jest!')).toBeInTheDocument();
-});
